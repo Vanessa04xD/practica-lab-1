6ca0abddd9baf21b912761c1f3f7bbf1036c8f63
Vanessa <69401828+Vanessa04xD@users.noreply.github.com>
2025-02-02 23:50:52 -0600

    Update index.php

--- index/index.php
+++ index/index.php
@@ -14,9 +14,9 @@
         <div class="card">
        
             <div class="card-body">
-               <ul>
+                <ul>
                     <li class="list-group-item">
-                       <p style="font-weight: bold; margin-bottom:0px;margin-top:5px;" >Ejemplos de la Guia</p>
+                       <p style="font-weight: bold; margin-bottom:0px;margin-top:5px;" >Ejemplos de la Guia 1</p>
                         <a href="/ej1/partehtml.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;" >Ejercicio 1</a><br>
                         <a href="/ej2/uso_variable.php" style="text-decoration: none ; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 2</a><br>
                         <a href="/ej3/opera_aritmeticos.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 3</a><br>
@@ -33,6 +33,23 @@
                         <a href="/disc/ej1/prueba.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 1</a><br>
                         <a href="/disc/ej2/form.html" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 1</a><br>
                     </li>
+               </ul>
+
+               <ul>
+                    <li class="list-group-item">
+                       <p style="font-weight: bold; margin-bottom:0px;margin-top:5px;" >Ejemplos de la Guia 2</p>
+                        <a href="/guia2/ej1/bisiesto.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;" >Ejercicio 1</a><br>
+                        <a href="/guia2/ej2/salario.php" style="text-decoration: none ; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 2</a><br>
+                        <a href="/guia2/ej3/compararnumeros.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 3</a><br>
+                        <a href="/guia2/ej4/convertir.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 4</a><br>
+                        <a href="/guia2/ej5/convmonedas.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejercicio 5</a>
+                        
+                    </li>
+                    <li class="list-group-item">
+                        <p style="font-weight: bold; margin-bottom:0px;margin-top:5px;"  >Ejemplo en Clase</p>
+                        <a href="/guia2/ejclase/index.php" style="text-decoration: none; color:rgb(26, 26, 26); padding-left:15px;">Ejemplo 1</a><br>
+                    </li>
+                   
                </ul>
             </div>
         </div>
