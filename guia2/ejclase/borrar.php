1cf76ef663c9cbbe23f53a3fb0033b17078c20eb
Vanessa <69401828+Vanessa04xD@users.noreply.github.com>
2025-02-02 23:50:02 -0600

    Añadiendo Guia2

---
+++ guia2/ejclase/borrar.php
@@ -0,0 +1,27 @@
+<?php
+if(isset($_GET['codigo']))
+{
+    $codigo=$_GET['codigo'];
+    echo $codigo;
+    $materias=simplexml_load_file('materias.xml');
+    $indexToDelete=-1;
+    $found=false;
+    foreach($materias->materia as $materia)
+    {
+        $indexToDelete++;
+        if($materia->codigo==$codigo)
+        {   
+            $found=true;
+            break;
+        }
+
+    }
+
+    if($found)
+    {
+        unset($materias->materia[$indexToDelete]);
+        file_put_contents('materias.xml',$materias->asXML());
+        header('location:index.php');
+    }
+}
+?>
